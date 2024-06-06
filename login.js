6ca659cc7a09afe2b2675a74085e9d53c6d34fc3
Muhammadizhan <izhanayaz4@gmail.com>
2024-06-06 14:59:55 +0500

    modified files

--- login.js
+++ login.js
@@ -1,3 +1,22 @@
+// document.addEventListener("DOMContentLoaded", () => {
+//   const loginForm = document.getElementById("loginForm");
+
+//   loginForm.addEventListener("submit", function (e) {
+//     e.preventDefault();
+//     const loginEmail = document.getElementById("loginEmail").value;
+//     const loginPassword = document.getElementById("loginPassword").value;
+
+//     const storedEmail = localStorage.getItem("email");
+//     const storedPassword = localStorage.getItem("password");
+
+//     if (loginEmail === storedEmail && loginPassword === storedPassword) {
+//       alert("Login successful");
+//       window.location.href = "dashboard.html";
+//     } else {
+//       alert("Invalid email or password");
+//     }
+//   });
+// });
 document.addEventListener("DOMContentLoaded", () => {
   const loginForm = document.getElementById("loginForm");
 
