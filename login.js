adcadd0182c5627e074586ef0e3051086bd109a8
Muhammadizhan <izhanayaz4@gmail.com>
2024-06-07 21:23:26 +0500

    some changes

--- login.js
+++ login.js
@@ -25,10 +25,17 @@ document.addEventListener("DOMContentLoaded", () => {
     const loginEmail = document.getElementById("loginEmail").value;
     const loginPassword = document.getElementById("loginPassword").value;
 
-    const storedEmail = localStorage.getItem("email");
-    const storedPassword = localStorage.getItem("password");
+    // Retrieve existing users from local storage
 
-    if (loginEmail === storedEmail && loginPassword === storedPassword) {
+    const users = JSON.parse(localStorage.getItem("users")) || [];
+
+    // Check if the email and password match any user
+
+    const user = users.find(
+      (user) => user.email === loginEmail && user.password === loginPassword
+    );
+
+    if (user) {
       alert("Login successful");
       window.location.href = "dashboard.html";
     } else {
